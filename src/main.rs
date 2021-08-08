3d02563ca9d192ef6afd1ff3940a9f2a119cdf2e
Adam Kelly <adam@cthulahoops.org>
2021-08-08 08:39:37 +0100

    Add simple sdl2 event loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use rand::prelude::*;
 use std::fs;
 
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
+
 struct Stack {
     values: [usize; 0x10],
     top: usize,
@@ -338,7 +343,25 @@ pub fn assemble(program: Vec<Instruction>) -> Vec<u8> {
     result
 }
 
+
 fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("CHIP-8", 64 * 10, 32 * 10)
+        .position_centered()
+        .build()
+        .unwrap();
+
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
     let mut vm = Vm::new();
 
     let program = fs::read("pong.ch8").unwrap();
@@ -350,6 +373,19 @@ fn main() {
 
     vm.load(program);
     loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => {
+                    return;
+                }
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    return;
+                }
+                _ => {}
+            }
+        }
+
         vm.step();
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
